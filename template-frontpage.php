33b9d04d92beb724aa876123205874239b1613b7
rednotice <jennifer.v.pfeil@gmail.com>
2020-04-04 14:00:56 +0200

    Homepage added

--- template-frontpage.php
+++ template-frontpage.php
@@ -8,13 +8,10 @@
 get_header(); ?>
 
 	<div class="woobits-section-1">
-		<div class="left">
+		<div class="promo">
 			<h1 class="title">We Help You Sell</h1>
 			<p class="description">We develop WooCommerce plugins that help you to simplify and grow your e-commerce business.</p>
 			<button class="call-to-action">Browse Plugins</button>
-		</div>
-		<div class="right">
-			<!-- <img src="<?php echo get_template_directory_uri(); ?>/src/img/woman_on_computer.png" alt="Woman on a computer"> -->
 		</div>
 	</div>
 
